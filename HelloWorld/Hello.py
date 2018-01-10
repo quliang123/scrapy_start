7e930ae3aee7c8d5465630294def0bb925431dcf
quliang123 <529075990@qq.com>
2018-01-10 09:37:45 +0800

    Python基础学习

---
+++ HelloWorld/Hello.py
@@ -0,0 +1,32 @@
+#helloworld
+print 'hello,python'
+
+
+print r'''...'''
+
+print u'''??'''
+
+
+print 2.5+10.0/4
+
+print '100+200='
+
+print 100+200
+
+
+a='python'
+print 'hello',a or 'python'
+
+b=''
+print 'hello','worlds' or 'world'
+
+
+l=[1,2,3,4,5]
+print l
+
+
+print l[-2]
+
+
+print len(l)
+
